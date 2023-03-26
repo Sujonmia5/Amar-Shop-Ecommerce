607903c55630388bcaefade0a7911ebfd977609e
Sujonmia5 <mdsujon673738@gmail.com>
2023-03-26 13:23:47 +0600

    first commit

---
+++ amar-shop-client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { RouterProvider } from 'react-router-dom';
+import './App.css'
+import { router } from './Router/Router';
+
+const App = () => {
+    return (
+        <RouterProvider router={router}>
+
+        </RouterProvider>
+    );
+};
+
+export default App;
